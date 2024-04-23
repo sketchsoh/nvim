ae7a4b7324813ed9e2639799fa3f5540f8bcbbf1
sketchsoh <sketchsoh@gmail.com>
2024-04-23 14:23:54 +0800

    updated options.lua

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -1,9 +1,9 @@
 return {
   {
-    "romgrk/barbar.nvim",
+    'romgrk/barbar.nvim',
     dependencies = {
-      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
     init = function()
       vim.g.barbar_auto_setup = false
@@ -13,7 +13,8 @@ return {
       -- animation = true,
       -- insert_at_start = true,
       -- …etc.
+      preset = 'powerline',
     },
-    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
 }
